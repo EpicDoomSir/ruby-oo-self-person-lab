a673946d88a0b8d5f18831de64821a151ae889e3
EpicDoomSir <eitands@gmail.com>
2020-10-07 11:00:00 -0400

    beggining

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
-# your code goes here
+require 'pry'
+
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(new_value)
+        @happiness = new_value.clamp(0, 10)
+    end
+
+    def hygiene=(new_value)
+        @hygiene = new_value.clamp(0, 10)
+    end
+
+end
+
+binding.pry
